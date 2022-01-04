3c537bee74d0e261168bdc44030e724e98dc4e31
Kam Patel <kam2perth@hotmail.com>
2022-01-04 21:18:46 +0000

    this is my fist commit

---
+++ manualsoftwareagenda.java
@@ -0,0 +1,13 @@
+package com.myfirstprogram.com;
+
+class ManualSoftware {
+
+    public static void main (String[] args) {
+        System.out.println("Week 1: What is Software, Risks & Benefits, who does testing, Qualifications, SDLC STLC");
+        System.out.println("Week 2: Challenges of manual testing, Principles of software testing, Test Plan, Test Scenario, test Case, Testing Techniques");
+        System.out.println("Week 3: Bug, Lifecycle Bug Template, Severity & Priority Testing Levels, White & Black Box testing");
+        System.out.println("Week 4: Static vs Dynamic testing, Exploratory testing, Ad-Hoc testing, Software models, Agile, Definition of Ready/Done");
+        System.out.println("Week 5: TDD, BDD, Maintenance");
+    }
+
+}
