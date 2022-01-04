2859da12c446ec7b97cf1d0879d6148f0922ad45
Kam Patel <kam2perth@hotmail.com>
2022-01-04 22:50:57 +0000

    change1

--- manualsoftwareagenda.java
+++ manualsoftwareagenda.java
@@ -7,7 +7,8 @@ class ManualSoftware {
         System.out.println("Week 2: Challenges of manual testing, Principles of software testing, Test Plan, Test Scenario, test Case, Testing Techniques");
         System.out.println("Week 3: Bug, Lifecycle Bug Template, Severity & Priority Testing Levels, White & Black Box testing");
         System.out.println("Week 4: Static vs Dynamic testing, Exploratory testing, Ad-Hoc testing, Software models, Agile, Definition of Ready/Done");
-        System.out.println("Week 5: TDD, BDD, Maintenance");
+        System.out.println("Week 5: TDD, BDD, Maintenance")
+        System.out.println("heyyyy");
     }
 
 }
